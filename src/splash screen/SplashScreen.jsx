147a5aab5e1db9387f4140ee713cb38e32c2b322
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2025-05-03 08:50:26 +0100

    add bg-color

--- src/splash screen/SplashScreen.jsx
+++ src/splash screen/SplashScreen.jsx
@@ -3,7 +3,7 @@ import "./SplashScreen.css"
 const SplashScreen = () => {
 
   return (
-    <section className="section d-flex justify-content-center align-items-center">
+    <section className="section d-flex justify-content-center align-items-center bg-color">
         <div className="splash_message-container">
             <img src="./eagle.png" alt="logo" className='splash-logo' />
             <p className='fw-bold fs-2'>QuickMart</p>
