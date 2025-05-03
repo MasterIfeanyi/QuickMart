2aa16a4f12f942ca86a08ac5173239a3a4c3f59c
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2025-05-03 09:34:11 +0100

    add brand variable

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Layout from './Layout/Layout'
 import SplashScreen from './Components/splash screen/SplashScreen'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import AuthScreen from './Components/auth/AuthScreen'
 
 function App() {
   
@@ -13,6 +14,7 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<SplashScreen />} />
+          <Route path="auth" element={<AuthScreen />} />
         </Routes>
       </Layout>
     </Router>
