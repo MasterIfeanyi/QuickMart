87e2596fef305e76ad3266fb89b20110fe3efe1a
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2025-05-03 08:40:28 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import SplashScreen from './splash screen/SplashScreen'
+
+function App() {
+  
+
+  return (
+    <main className='App'>
+      <SplashScreen />
+    </main>
+  )
+}
+
+export default App
---
+++ src/splash screen/SplashScreen.jsx
@@ -0,0 +1,21 @@
+import "./SplashScreen.css"
+
+const SplashScreen = () => {
+
+  return (
+    <section className="section d-flex justify-content-center align-items-center">
+        <div className="splash_message-container">
+            <img src="./eagle.png" alt="logo" className='splash-logo' />
+            <p className='fw-bold fs-2'>QuickMart</p>
+        </div>
+        {/* <div className="container">
+            <div className="row">
+                <div className="align-items-center justify-content-center">
+                </div>
+            </div>
+        </div> */}
+    </section>
+  )
+}
+
+export default SplashScreen
