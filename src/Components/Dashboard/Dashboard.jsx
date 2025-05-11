4b2816d8aad659384cc4f27d45878a44a32e6380
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2025-05-11 19:56:43 +0100

    fix sidebar

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -31,14 +31,15 @@ const Dashboard = () => {
         <div className="container-fluid p-0">
             <div className="row g-0">
                 {/* Sidebar Navigation */}
-                <nav className='col-md-3 col-lg-2 d-md-block sidebar'>
-                    <div className="position-sticky">
+                <nav className='col-md-3 col-lg-3 d-md-block sidebar'>
+                    <div className="position-sticky sidebar-container">
                         <div className="sidebar-logo">
                             <FiShoppingBag className="splash-logo mb-3" size={20} color="white" />
                             <h3 className='sidebar-title'>QuickMart</h3>
                         </div>
-                        <ul className="nav category-btns">
-                            <div className="">
+                        
+                        <div className="sidebar-content">
+                            <ul className="nav category-btns">
                                 <li className="nav-item">
                                     <a href="" className={`nav-link ${selectedCategory === "Fashion" ? "active" : ""}`} onClick={(e) => handleCategorySelect("Fashion", e)}>
                                         Fashion
@@ -56,18 +57,17 @@ const Dashboard = () => {
                                         Beauty
                                     </a>
                                 </li>
-                            </div>
-                            <div>
-                                <a href="" className="btn btn-brand">Logout</a>
-                            </div>
-                        </ul>
+                            </ul>
+                            <a href="" className="btn btn-brand">Logout</a>
+                        </div>
+                        
                     </div>
                 </nav>
 
 
 
                 {/* Main content */}
-                <main className="col-lg-9 col-md-10 px-4 py-2 main-content">
+                <main className="col-lg-8 col-md-10 px-4 py-2 main-content">
 
                     <div className="row g-4">
 
